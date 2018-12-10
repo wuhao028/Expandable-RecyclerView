f465b99062b44214761b72442c45c718c88770ac
wuhao028 <wuhao028@gmail.com>
2018-12-10 21:25:41 +0800

    make it full screen

--- app/src/main/java/com/wuhao028/pokedex/ui/SplashActivity.kt
+++ app/src/main/java/com/wuhao028/pokedex/ui/SplashActivity.kt
@@ -3,11 +3,15 @@ package com.wuhao028.pokedex.ui
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Window
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
 import com.wuhao028.pokedex.R
 import kotlinx.android.synthetic.main.activity_splash.*
 import java.util.*
+import android.view.WindowManager
+
+
 
 /**
  *Created by WuHao028 on 4/11/18
@@ -19,8 +23,12 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         this.getSupportActionBar()?.hide()
         setContentView(R.layout.activity_splash)
+        val window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         var welcomePics = arrayOf(R.drawable.welcomeone, R.drawable.welcometwo,
                 R.drawable.welcomethree, R.drawable.welcomefour, R.drawable.welcomefive)
 
